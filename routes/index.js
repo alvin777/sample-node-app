4e351535333a85526ee208cd09567202be1cc729
Stanislav Krasnoyarov <stanislav.krasnoyarov@gmail.com>
2016-06-21 16:10:58 +0300

    Implemented health endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const sequelize = require('../models').sequelize;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/health', function(req, res, next) {
+  sequelize.authenticate().then( errors => {
+    console.log(errors)
+    res.json({
+      status: 'OK'
+    });
+  }).catch( e => {
+    res.status(500);
+    res.json({
+      status: 'FAIL'
+    });
+  });
 });
 
 module.exports = router;
